d63f230e147b063e699e782c4c1dc15ce7f37f56
Kyle Stewart <kylestew@gmail.com>
2020-05-17 12:58:59 -0700

    shitty saw wave demo from rtaudio

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,18 @@
 int saw(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
         double streamTime, RtAudioStreamStatus status, void *userData) {
 
-    // TODO:....
+    unsigned int i, j;
+    double *buffer = (double *)outputBuffer;
+    double *lastValues = (double *)userData;
+
+    for (i = 0; i < nBufferFrames; i++) {
+        for (j = 0; j < 2; j++) {
+            *buffer++ = lastValues[j];
+
+            lastValues[j] += 0.005 * (j + 1 + (j * 0.1));
+            if (lastValues[j] >= 1.0) lastValues[j] -= 2.0;
+        }
+    }
 
     return 0;
 }
@@ -25,22 +36,26 @@ int main() {
     params.firstChannel = 0;
     unsigned int sampleRate = 44100;
     unsigned int bufferFrames = 256;
-    double data[2];
+    double userData[2];
 
     try {
-        dac.openStream(&params, NULL, RTAUDIO_FLOAT64, sampleRate, &bufferFrames, &saw, (void*)&data);
+        dac.openStream(&params, NULL, RTAUDIO_FLOAT64, sampleRate, &bufferFrames, &saw, (void*)&userData);
         dac.startStream();
     } catch (RtAudioError &e) {
         e.printMessage();
         exit(0);
     }
 
+    char input;
+    std::cout << "\nPlaying ... press <enter> to quit.\n";
+    std::cin.get(input);
 
-
-
-
-
-
+    try {
+        // stop the stream
+        dac.stopStream();
+    } catch (RtAudioError &e) {
+        e.printMessage();
+    }
 
     if (dac.isStreamOpen()) dac.closeStream();
 
