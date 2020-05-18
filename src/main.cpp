5047b4814281a608251e8d81d6ddd3dbbf5c1b9e
Kyle Stewart <kylestew@gmail.com>
2020-05-17 18:04:57 -0700

    table based very simple sine wave

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,42 @@
 #include "rtaudio/RtAudio.h"
 #include <cstdlib>
+#include <cmath>
 #include <iostream>
+#include <vector>
 
 
-int saw(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
+float gFrequency = 220.0;
+float gAmplitude = 0.6;
+float gDuration = 5.0;
+int gNumSamples = 0;
+std::vector<float> gSampleData;
+
+
+void calculate_sine(float *buffer, int numSamples, float sampleRate) {
+    for (int n = 0; n < numSamples; n++) {
+        float out = gAmplitude * sin(2.0 * M_PI * n * gFrequency / sampleRate);
+        buffer[n] = out;
+    }
+}
+
+
+int render(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
         double streamTime, RtAudioStreamStatus status, void *userData) {
 
     unsigned int i, j;
     double *buffer = (double *)outputBuffer;
-    double *lastValues = (double *)userData;
 
+    int idx = *(int *)userData;
     for (i = 0; i < nBufferFrames; i++) {
         for (j = 0; j < 2; j++) {
-            *buffer++ = lastValues[j];
-
-            lastValues[j] += 0.005 * (j + 1 + (j * 0.1));
-            if (lastValues[j] >= 1.0) lastValues[j] -= 2.0;
+            *buffer++ = gSampleData[idx];
         }
+        idx++;
+        if (idx >= gSampleData.size())
+            idx = 0;
     }
+
+    *(int *)userData = idx;
 
     return 0;
 }
@@ -36,10 +55,17 @@ int main() {
     params.firstChannel = 0;
     unsigned int sampleRate = 44100;
     unsigned int bufferFrames = 256;
-    double userData[2];
+    int tableIndex;
+
+
+    // pre-calc sine data - enough for one cycle
+    gNumSamples = static_cast<int>(sampleRate / gFrequency);
+    gSampleData.resize(gNumSamples);
+    calculate_sine(gSampleData.data(), gNumSamples, sampleRate);
+
 
     try {
-        dac.openStream(&params, NULL, RTAUDIO_FLOAT64, sampleRate, &bufferFrames, &saw, (void*)&userData);
+        dac.openStream(&params, NULL, RTAUDIO_FLOAT64, sampleRate, &bufferFrames, &render, (void*)&tableIndex);
         dac.startStream();
     } catch (RtAudioError &e) {
         e.printMessage();
